ead872642c66a46805aeba0c4d0345cb347f6f93
nooraiym <140681537+nooraiym@users.noreply.github.com>
2024-10-04 19:11:01 +0500

    feat: add tasks display

---
+++ src/app/tasks/task/task.component.ts
@@ -0,0 +1,15 @@
+import { DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { Task } from '../../../data/tasksData';
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [DatePipe, MatCardModule, MatCheckboxModule],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.scss',
+})
+export class TaskComponent {
+  @Input({ required: true }) task!: Task;
+}
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../../data/tasksData';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.scss'
+  styleUrl: './tasks.component.scss',
 })
 export class TasksComponent {
-  @Input({required: true}) tasks!: Task[];
+  @Input({ required: true }) tasks!: Task[];
 }
